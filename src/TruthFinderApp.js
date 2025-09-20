51f6886a0b3d84ab333e502fb09b6fb7b023e6d6
genaiexchangegit <yolaliv773@gmail.com>
2025-09-20 23:34:47 +0530

    Update TruthFinderApp.js

--- src/TruthFinderApp.js
+++ src/TruthFinderApp.js
@@ -18,7 +18,7 @@ const TruthFinderApp = () => {
     setAnalysis(null);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/analyze', {
+      const response = await fetch('https://truthfinderai-backend.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
