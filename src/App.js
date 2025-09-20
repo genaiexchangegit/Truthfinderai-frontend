040189f4ba099abb2baf64d33725f0ecc335c384
genaiexchangegit <yolaliv773@gmail.com>
2025-09-20 23:23:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,4 @@ import TruthFinderApp from './TruthFinderApp';
 function App() {
   return <TruthFinderApp />;
 }
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> 5a0ad37de4013c04b494d31d6b524a26a06de68c
